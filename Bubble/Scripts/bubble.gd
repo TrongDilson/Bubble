f332fa68e902faf1cf871f3742ae10a097b6e9e7
TrongDilson <108319460+TrongDilson@users.noreply.github.com>
2025-01-18 14:42:25 +0700

    update state

--- Bubble/Scripts/bubble.gd
+++ Bubble/Scripts/bubble.gd
@@ -1,25 +1,24 @@
 extends CharacterBody2D
 
 @onready var joystick = $"../Camera2D/joystick"
+@onready var anim_player = $AnimatedSprite2D
 
 var speed = 30
 
+# Reference to the current state
+var current_state: State
+
+func _ready():
+	# Start in the Idle state
+	set_state(idle_state.new(self))
+
+func set_state(new_state: State):
+	if current_state:
+		current_state.Exit()  # Exit the current state
+	current_state = new_state
+	current_state.Enter()  # Enter the new state
+
 func _physics_process(delta):
-	# Initialize direction based on joystick input
-	var direction = joystick.posVector
-
-	# Apply gravity only to the y-component of velocity
-	if not is_on_floor():
-		velocity.y += get_gravity().y * delta  # Add gravity effect
-	else:
-		velocity.y = 0  # Reset vertical velocity if on the floor
-
-	# Add directional movement based on joystick input
-	if direction:
-		velocity.x = direction.x * speed * 10
-		velocity.y += direction.y * speed # Vertical movement from joystick input
-	else:
-		velocity.x = 0  # Stop horizontal movement when no input
-	
-	# Move the character with the calculated velocity
-	move_and_slide()
+	# Let the current state handle updates
+	if current_state:
+		current_state.Update(delta)
